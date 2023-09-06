f15ac072b1cd6f2ebf763c41292ccee767e0255b
krone <krone@krone.krone>
2023-09-06 08:31:23 -0300

    a head for our linked list

--- src/main.c
+++ src/main.c
@@ -39,8 +39,10 @@ struct block_meta *find_free_block(struct block_meta **last size_t size) {
   }
   return current;
 
-
 }
+
+
+void *global_base = NULL;
 
 struct block_meta *request_space(struct block_meta* last,size_t size){
 
