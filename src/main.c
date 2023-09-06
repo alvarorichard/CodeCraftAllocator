832bc4d8bd9ce4bf7ad0c9374f3b89312623eab0
alvarorichard <alvaro10048@protonmail.com>
2023-09-06 19:35:55 -0300

    improve somethings

--- src/main.c
+++ src/main.c
@@ -3,20 +3,21 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-
+// Estrutura de metadados para os blocos alocados
 struct block_meta {
   size_t size;
   struct block_meta *next;
   int free;
-  int magic; // For debugging only
-  char data[1]; 
+  int magic; 
 };
 
 #define META_SIZE sizeof(struct block_meta)
 
+// Ponteiro para o início da lista de blocos alocados
 void *global_base = NULL;
 
-void *find_free_block(struct block_meta **last, size_t size) {
+// Encontra um bloco livre de tamanho 'size' na lista
+struct block_meta *find_free_block(struct block_meta **last, size_t size) {
   struct block_meta *current = global_base;
   while (current && !(current->free && current->size >= size)) {
     *last = current;
@@ -25,11 +26,11 @@ void *find_free_block(struct block_meta **last, size_t size) {
   return current;
 }
 
-void *request_space(struct block_meta *last, size_t size) {
+// Aloca espaço para um novo bloco
+struct block_meta *request_space(struct block_meta *last, size_t size) {
   struct block_meta *block;
   block = sbrk(0);
   void *request = sbrk(size + META_SIZE);
-  assert((void *)block == request);
   if (request == (void *)-1) {
     return NULL;
   }
@@ -37,6 +38,7 @@ void *request_space(struct block_meta *last, size_t size) {
   if (last) {
     last->next = block;
   }
+
   block->size = size;
   block->next = NULL;
   block->free = 0;
@@ -44,6 +46,7 @@ void *request_space(struct block_meta *last, size_t size) {
   return block;
 }
 
+// Aloca memória
 void *my_malloc(size_t size) {
   struct block_meta *block;
 
@@ -74,70 +77,57 @@ void *my_malloc(size_t size) {
   return (block + 1);
 }
 
+// Obtém o ponteiro de bloco a partir do ponteiro de dados
 struct block_meta *get_block_ptr(void *ptr) {
-  return (struct block_meta*)ptr - 1;
+  return (struct block_meta *)ptr - 1;
 }
 
-
+// Libera memória
 void my_free(void *ptr) {
   if (!ptr) {
     return;
   }
 
-  struct block_meta *block = (struct block_meta *)ptr - 1;
+  struct block_meta *block = get_block_ptr(ptr);
   assert(block->free == 0);
   block->free = 1;
-  struct block_meta* block_ptr = get_block_ptr(ptr);
-  assert(block_ptr->free == 0);
-  assert(block_ptr->magic == 0x77777777 || block_ptr->magic == 0x12345678);
-  block_ptr->free = 1;
-  block_ptr->magic = 0x55555555;
-  
+  block->magic = 0x55555555;
 }
 
-
-
-void *realloc(void *ptr, size_t size) {
+// Redimensiona a memória alocada
+void *my_realloc(void *ptr, size_t size) {
   if (!ptr) {
     return my_malloc(size);
   }
 
-  struct block_meta* block_ptr = get_block_ptr(ptr);
+  struct block_meta *block_ptr = get_block_ptr(ptr);
   if (block_ptr->size >= size) {
-    // We have enough space. Could free some once we implement split.
+    // Temos espaço suficiente. Poderíamos liberar algum espaço quando implementarmos a divisão.
     return ptr;
   }
 
-  // Need to really realloc. Malloc new space and free old space.
-  // Then copy old data to new space.
-  void *new_ptr;
-  new_ptr = my_malloc(size);
+  // Precisamos realmente realocar. Alocar novo espaço e liberar o espaço antigo.
+  // Em seguida, copiar os dados antigos para o novo espaço.
+  void *new_ptr = my_malloc(size);
   if (!new_ptr) {
-    return NULL; // TODO: set errno on failure.
+    return NULL; // TODO: definir errno em caso de falha.
   }
   memcpy(new_ptr, ptr, block_ptr->size);
   my_free(ptr);
   return new_ptr;
 }
 
-void *calloc(size_t nelem, size_t elsize) {
-  size_t size = nelem * elsize; 
+// Aloca memória e a inicializa com zero
+void *my_calloc(size_t nelem, size_t elsize) {
+  size_t size = nelem * elsize;
   void *ptr = my_malloc(size);
-  memset(ptr, 0, size);
+  if (ptr) {
+    memset(ptr, 0, size);
+  }
   return ptr;
 }
 
-int main(){
- 
+int main() {
+  // Teste suas funções de alocação e desalocação de memória aqui
   return 0;
 }
-
-
-
-
-
-
-
-
-
-
