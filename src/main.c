c9dadaa565e2e86e01c3768103adfe571bec827d
alvarorichard <alvaro10048@protonmail.com>
2023-09-06 19:02:18 -0300

    fix

--- src/main.c
+++ src/main.c
@@ -3,93 +3,86 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-void * malloc(size_t size){
-
-  void *p = sbrk(0);
-  void *request = sbrk(size);
-  if (request == (void*) -1) {
-    return NULL;
-  } else {
-    assert(p == request);
-    return p;
-  }
-}
-
 struct block_meta {
-  size_t;
+  size_t size;
   struct block_meta *next;
   int free;
-  int magic;
+  int magic; // For debugging only. TODO: remove this in non-debug mode.
 };
 
-
-#define META_SIZE sizeof (struct block_meta)
+#define META_SIZE sizeof(struct block_meta)
 
 void *global_base = NULL;
 
-
-struct block_meta *find_free_block(struct block_meta **last size_t size) {
-
+void *find_free_block(struct block_meta **last, size_t size) {
   struct block_meta *current = global_base;
-
-  while (current && ! (current -> free && current ->size >=size)) {
-
+  while (current && !(current->free && current->size >= size)) {
     *last = current;
-    current = current -> next;
+    current = current->next;
   }
   return current;
-
 }
 
-
-void *global_base = NULL;
-
-struct block_meta *request_space(struct block_meta* last,size_t size){
-
+void *request_space(struct block_meta *last, size_t size) {
   struct block_meta *block;
   block = sbrk(0);
   void *request = sbrk(size + META_SIZE);
-  assert((void*) block == request);
-  if (request == (void*) -1) {
+  assert((void *)block == request);
+  if (request == (void *)-1) {
     return NULL;
   }
+
   if (last) {
-    last -> next = block;
+    last->next = block;
   }
-  block -> size = size;
-  block -> next = NULL;
-  block -> free = 0;
-  block -> magic = 0x12345678;
+  block->size = size;
+  block->next = NULL;
+  block->free = 0;
+  block->magic = 0x12345678;
   return block;
 }
 
-void *malloc(size_t size){
-
+void *my_malloc(size_t size) {
   struct block_meta *block;
 
   if (size <= 0) {
     return NULL;
   }
 
-  global_base = block;
-
-  else {
-    struct block_meta *last = global_base;
+  if (!global_base) {
+    block = request_space(NULL, size);
     if (!block) {
-      block = request_space(last,size);
-      if (!block) {
-        return NULLL;
-      }
-
+      return NULL;
     }
-    else {
-      block -> free = 0;
-      block -> magic = 0x77777777;
+    global_base = block;
+  } else {
+    struct block_meta *last = global_base;
+    block = find_free_block(&last, size);
+    if (!block) {
+      block = request_space(last, size);
+      if (!block) {
+        return NULL;
+      }
+    } else {
+      block->free = 0;
+      block->magic = 0x77777777;
     }
   }
-  return (block+1);
+
+  return (block + 1);
 }
 
+void my_free(void *ptr) {
+  if (!ptr) {
+    return;
+  }
 
+  struct block_meta *block = (struct block_meta *)ptr - 1;
+  assert(block->free == 0);
+  block->free = 1;
+}
 
-
+int main() {
+  // Test your memory allocation and deallocation here
+  return 0;
+}
