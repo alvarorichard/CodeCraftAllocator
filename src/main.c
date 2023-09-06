90c123d3d06b16634444ab911f12da7540f21d66
alvarorichard <alvaro10048@protonmail.com>
2023-09-05 22:51:12 -0300

    add struct

--- src/main.c
+++ src/main.c
@@ -14,3 +14,31 @@ void * malloc(size_t size){
     return p;
   }
 }
+
+struct block_meta {
+  size_t;
+  struct block_meta *next;
+  int free;
+  int magic;
+};
+
+
+#define META_SIZE sizeof (struct block_meta)
+
+void *global_base = NULL;
+
+
+struct block_meta *find_free_block(struct block_meta **last size_t size) {
+
+  struct block_meta *current = global_base;
+
+  while (current && ! (current -> free && current ->size >=size)) {
+
+    *last = current;
+    current = current -> next;
+  }
+  return current;
+
+
+}
+
